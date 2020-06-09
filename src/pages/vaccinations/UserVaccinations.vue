25fb0f5057fcd53a8f4e44791ae672041beae874
lericardolima <lericardolima@gmail.com>
2020-06-09 10:57:15 -0300

    Filtrar registro pela data

--- src/pages/vaccinations/UserVaccinations.vue
+++ src/pages/vaccinations/UserVaccinations.vue
@@ -11,9 +11,61 @@
                 label="Pesquisar"
                 stack-label
                 :dense="true"
-                v-model="query"
+                v-model="search.query"
                 @keyup="findAllVaccinations"
               />
+            </div>
+            <div class="row wrap q-gutter-sm">
+              <q-input
+                class="col col-xs-5 col-lg-4"
+                outlined
+                label="De"
+                stack-label
+                :dense="true"
+                v-model="search.from"
+              >
+                <template v-slot:append>
+                  <q-icon name="event" class="cursor-pointer">
+                    <q-popup-proxy
+                      ref="qDateFromProxy"
+                      transition-show="scale"
+                      transition-hide="scale"
+                    >
+                      <q-date
+                        :mask="dateUtils.MASK"
+                        v-model="search.from"
+                        :locale="dateUtils.LOCALE"
+                        @input="dateChanged($refs.qDateFromProxy)"
+                      />
+                    </q-popup-proxy>
+                  </q-icon>
+                </template>
+              </q-input>
+              <q-input
+                class="col col-xs-5 col-lg-4"
+                outlined
+                label="Até"
+                stack-label
+                :dense="true"
+                v-model="search.to"
+              >
+                <template v-slot:append>
+                  <q-icon name="event" class="cursor-pointer">
+                    <q-popup-proxy
+                      ref="qDateToProxy"
+                      transition-show="scale"
+                      transition-hide="scale"
+                    >
+                      <q-date
+                        :mask="dateUtils.MASK"
+                        v-model="search.to"
+                        :locale="dateUtils.LOCALE"
+                        @input="dateChanged($refs.qDateToProxy)"
+                      />
+                    </q-popup-proxy>
+                  </q-icon>
+                </template>
+              </q-input>
             </div>
           </div>
         </q-form>
@@ -55,11 +107,17 @@
 <script>
 import { mapGetters } from 'vuex';
 import types from '../../store/types';
+import dateUtils from '../../shared/utils/date';
 
 export default {
   data() {
     return {
-      query: '',
+      dateUtils,
+      search: {
+        query: '',
+        from: '',
+        to: '',
+      },
       vaccinations: [],
     };
   },
@@ -70,14 +128,26 @@ export default {
   },
   methods: {
     findAllVaccinations() {
-      let url = `/api/users/${this.user.id}/vaccinations`;
-      if (this.query) {
-        url += `?q=${this.query}`;
+      let url = `/api/users/${this.user.id}/vaccinations?`;
+      if (this.search.query) {
+        url += `q=${this.search.query}&`;
+      }
+
+      if (this.search.from) {
+        url += `from=${dateUtils.toString(this.search.from)}&`;
+      }
+
+      if (this.search.to) {
+        url += `to=${dateUtils.toString(this.search.to)}&`;
       }
 
       this.$axios.get(url).then((response) => {
         this.vaccinations = response.data;
       });
+    },
+    dateChanged(qDateProxy) {
+      this.findAllVaccinations();
+      qDateProxy.hide();
     },
   },
   mounted() {
