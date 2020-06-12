f2c6bca7e7e7949ad3ae5f3fc12d8d1ef126d5ba
lericardolima <lericardolima@gmail.com>
2020-06-12 12:09:40 -0300

    Validar inputs do usuário na tela do mural

--- src/pages/newsfeed/NewsfeedData.vue
+++ src/pages/newsfeed/NewsfeedData.vue
@@ -19,8 +19,13 @@
                 label="Titulo"
                 stack-label
                 :dense="true"
-                v-model="newsfeed.title"
-              />
+                v-model="post.title"
+                :error="isTitleInvalid"
+              >
+                <template v-slot:error>
+                  <p v-if="!isOkTitleRequired">* Campo obrigatório.</p>
+                </template>
+              </q-input>
             </div>
             <div class="row wrap">
               <q-input
@@ -30,8 +35,13 @@
                 type="textarea"
                 stack-label
                 :dense="true"
-                v-model="newsfeed.description"
-              />
+                v-model="post.description"
+                :error="isDescriptionInvalid"
+              >
+                <template v-slot:error>
+                  <p v-if="!isOkDescriptionRequired">* Campo obrigatório.</p>
+                </template>
+              </q-input>
             </div>
             <div class="row wrap">
               <q-input
@@ -40,7 +50,8 @@
                 label="Link"
                 stack-label
                 :dense="true"
-                v-model="newsfeed.link"
+                v-model="post.link"
+                :error="isLinkInvalid"
               />
             </div>
             <div class="row wrap">
@@ -50,6 +61,8 @@
                 label="Data de expiração"
                 stack-label
                 :dense="true"
+                v-model="post.expiresAt"
+                :error="isExpiresAtInvalid"
               >
                 <template v-slot:append>
                   <q-icon name="event" class="cursor-pointer">
@@ -60,12 +73,16 @@
                     >
                       <q-date
                         :mask="dateUtils.MASK"
-                        v-model="newsfeed.expiresAt"
+                        v-model="post.expiresAt"
                         :locale="dateUtils.LOCALE"
                         @input="() => $refs.qDateProxy.hide()"
                       />
                     </q-popup-proxy>
                   </q-icon>
+                </template>
+
+                <template v-slot:error>
+                  <p v-if="!isOkExpiresAtMinValue">* Somente datas futuras.</p>
                 </template>
               </q-input>
             </div>
@@ -74,13 +91,20 @@
                 label="Exibir ícone de notificação?"
                 :dense="true"
                 color="accent"
-                v-model="newsfeed.showIcon"
+                v-model="post.showIcon"
+                :error="isShowIconInvalid"
               />
             </div>
           </q-form>
         </q-card-section>
         <div class="q-my-lg row justify-center">
-          <q-btn label="Criar" class="q-mt-md" color="teal" @click="create">
+          <q-btn
+            :disable="isPostInvalid"
+            label="Criar"
+            class="q-mt-md"
+            color="teal"
+            @click="create"
+          >
             <template v-slot:loading>
               <q-spinner-facebook />
             </template>
@@ -89,13 +113,13 @@
         <alert-error ref="alertError">
           <p slot="title">Algo deu errado!</p>
           <p slot="message">
-            Infelizmente não foi possivel adicionar a noticia no mural!
+            Infelizmente não foi possivel adicionar o post no mural!
           </p>
         </alert-error>
-        <alert-success ref="alertSuccess">
+        <alert-success ref="alertSuccess" @onClickOK="backToNewsfeed">
           <p slot="title">Sucesso!</p>
           <p slot="message">
-            A notícia foi adicionada com sucesso!
+            O post foi adicionado com sucesso!
           </p>
         </alert-success>
       </q-card-section>
@@ -103,6 +127,7 @@
   </div>
 </template>
 <script>
+import { required } from 'vuelidate/lib/validators';
 import dateUtils from '../../shared/utils/date';
 import AlertError from '../../components/shared/alert/Error';
 import AlertSuccess from '../../components/shared/alert/Success';
@@ -115,7 +140,7 @@ export default {
   data() {
     return {
       dateUtils,
-      newsfeed: {
+      post: {
         title: '',
         description: '',
         link: '',
@@ -126,32 +151,95 @@ export default {
   },
   methods: {
     create() {
-      this.newsfeed = {
-        title: this.newsfeed.title,
-        description: this.newsfeed.description,
-        link: this.newsfeed.link,
-        expiresAt: dateUtils.toString(this.newsfeed.expiresAt),
-        showIcon: this.newsfeed.showIcon,
+      this.post = {
+        title: this.post.title,
+        description: this.post.description,
+        link: this.post.link,
+        expiresAt: dateUtils.toString(this.post.expiresAt),
+        showIcon: this.post.showIcon,
       };
 
       this.$axios
-        .post('/api/newsfeed', this.newsfeed)
+        .post('/api/newsfeed', this.post)
         .then(() => {
           this.$refs.alertSuccess.open();
-          // eslint-disable-next-line no-console
-          console.log(this.newsfeed);
-          this.newsfeed = {
-            title: '',
-            description: '',
-            link: '',
-            expiresAt: '',
-          };
         })
         .catch(() => {
-          // eslint-disable-next-line no-console
-          console.log(this.newsfeed);
           this.$refs.alertError.open();
         });
+    },
+    backToNewsfeed() {
+      this.$router.back();
+    },
+  },
+  computed: {
+    isPostInvalid() {
+      return this.$v.post.$invalid;
+    },
+    isTitleInvalid() {
+      return this.$v.post.title.$invalid;
+    },
+    isDescriptionInvalid() {
+      return this.$v.post.description.$invalid;
+    },
+    isLinkInvalid() {
+      return this.$v.post.link.$invalid;
+    },
+    isExpiresAtInvalid() {
+      return this.$v.post.expiresAt.$invalid;
+    },
+    isShowIconInvalid() {
+      return this.$v.post.showIcon.$invalid;
+    },
+    isOkTitleRequired() {
+      return this.$v.post.title.required;
+    },
+    isOkDescriptionRequired() {
+      return this.$v.post.description.required;
+    },
+    isOkExpiresAtMinValue() {
+      return this.$v.post.showIcon.minValue;
+    },
+    isOkShowIconRequired() {
+      return this.$v.post.showIcon.required;
+    },
+  },
+  validations: {
+    post: {
+      title: {
+        required,
+      },
+      description: {
+        required,
+      },
+      link: {},
+      expiresAt: {
+        minValue(value) {
+          if (value) {
+            const fields = [...value.split('/')];
+            const expiration = new Date();
+            expiration.setDate(fields[0]);
+            expiration.setMonth(fields[1] - 1);
+            expiration.setYear(fields[2]);
+            expiration.setHours(0);
+            expiration.setMinutes(0);
+            expiration.setSeconds(0);
+            expiration.setMilliseconds(0);
+
+            const today = new Date();
+            today.setHours(0);
+            today.setMinutes(0);
+            today.setSeconds(0);
+            today.setMilliseconds(0);
+            return expiration > today;
+          }
+
+          return true;
+        },
+      },
+      showIcon: {
+        required,
+      },
     },
   },
 };
