487b49fefd2db5e153f1eb518cc201ec5f76499d
lericardolima <lericardolima@gmail.com>
2020-06-11 23:06:26 -0300

    Aplicar a arte milenar da identação

--- src/pages/newsfeed/NewsfeedData.vue
+++ src/pages/newsfeed/NewsfeedData.vue
@@ -2,108 +2,95 @@
   <div>
     <q-card class="q-pa-xl min">
       <q-card-section>
-    <q-card-section class="q-pt-none q-gutter-sm">
-      <router-link class="text-primary" to="/site/newsfeed" tag="a">
-        Voltar
-      </router-link>
-      <p class="text-h6 text-dark text-center svd-title">
-        Adicionar nova notícia no mural
-      </p>
-    </q-card-section>
-      <q-form class="row justify-center" >
-        <div class="col col-10 q-col-gutter-sm">
-          <div class="corpo">
-            <label class="col col-xs-12 col-md-10">Titulo
-            <q-input
-              outlined
-              :dense="true"
-              v-model="newsfeed.title"
-            >
-            </q-input>
-            </label>
+        <q-card-section class="q-pt-none q-gutter-sm">
+          <router-link class="text-primary" to="/site/newsfeed" tag="a">
+            Voltar
+          </router-link>
+          <p class="text-h6 text-dark text-center svd-title">
+            Adicionar nova notícia no mural
+          </p>
+        </q-card-section>
+        <q-form class="row justify-center">
+          <div class="col col-10 q-col-gutter-sm">
+            <div class="corpo">
+              <label class="col col-xs-12 col-md-10"
+                >Titulo
+                <q-input outlined :dense="true" v-model="newsfeed.title">
+                </q-input>
+              </label>
+            </div>
           </div>
-        </div>
-      </q-form>
-      <q-form class="row justify-center" >
-        <div class="col col-10 q-col-gutter-sm">
-          <div class="corpo">
-            <label class="col col-xs-12 col-md-10">Descrição
-             <q-input
-              outlined
-              :dense="true"
-              v-model="newsfeed.description"
-            >
-            </q-input>
-            </label>
+        </q-form>
+        <q-form class="row justify-center">
+          <div class="col col-10 q-col-gutter-sm">
+            <div class="corpo">
+              <label class="col col-xs-12 col-md-10"
+                >Descrição
+                <q-input outlined :dense="true" v-model="newsfeed.description">
+                </q-input>
+              </label>
+            </div>
           </div>
-        </div>
-      </q-form>
-      <q-form class="row justify-center" >
-        <div class="col col-10 q-col-gutter-sm">
-          <div class="corpo">
-            <label class="col col-xs-12 col-md-10">Link
-            <q-input
-              outlined
-              :dense="true"
-              v-model="newsfeed.link"
-            >
-            </q-input>
-            </label>
+        </q-form>
+        <q-form class="row justify-center">
+          <div class="col col-10 q-col-gutter-sm">
+            <div class="corpo">
+              <label class="col col-xs-12 col-md-10"
+                >Link
+                <q-input outlined :dense="true" v-model="newsfeed.link">
+                </q-input>
+              </label>
+            </div>
           </div>
-        </div>
-      </q-form>
-      <q-form class="row justify-left" >
-        <div class="col col-5 q-col-gutter-sm">
-          <div class="data-experacao">
-            <label class="col col-xs-2 col-md-10">Data de expiração
-               <q-input
-                outlined
-              :dense="true"
-                type="date"
-                :locale="dateUtils.LOCALE"
-                v-model="newsfeed.expiresAt"
-                  />
-            </label>
+        </q-form>
+        <q-form class="row justify-left">
+          <div class="col col-5 q-col-gutter-sm">
+            <div class="data-experacao">
+              <label class="col col-xs-2 col-md-10"
+                >Data de expiração
+                <q-input
+                  outlined
+                  :dense="true"
+                  type="date"
+                  :locale="dateUtils.LOCALE"
+                  v-model="newsfeed.expiresAt"
+                />
+              </label>
+            </div>
           </div>
-        </div>
-      </q-form>
-      <q-form class="row justify-left" >
-        <div class="col col-10 q-col-gutter-sm">
-          <div class="check-box">
-            <q-checkbox
-            label="Exibir ícone de notificação?"
-            :dense="true"
-             color="accent"
-             v-model="newsfeed.showIcon"
-           />
+        </q-form>
+        <q-form class="row justify-left">
+          <div class="col col-10 q-col-gutter-sm">
+            <div class="check-box">
+              <q-checkbox
+                label="Exibir ícone de notificação?"
+                :dense="true"
+                color="accent"
+                v-model="newsfeed.showIcon"
+              />
+            </div>
           </div>
+        </q-form>
+        <div class="btnCadastrar row justify-center">
+          <q-btn label="Criar" class="q-mt-md" color="teal" @click="create">
+            <template v-slot:loading>
+              <q-spinner-facebook />
+            </template>
+          </q-btn>
         </div>
-      </q-form>
-    <div class="btnCadastrar row justify-center">
-    <q-btn
-        label="Criar"
-        class="q-mt-md"
-        color="teal"
-        @click="create"
-        >
-        <template v-slot:loading>
-          <q-spinner-facebook />
-        </template>
-      </q-btn>
-    </div>
         <alert-error ref="alertError">
-      <p slot="title">Algo deu errado!</p>
-      <p slot="message">
-        Infelizmente não foi possivel adicionar a noticia no mural!
-      </p>
-    </alert-error>
-    <alert-success ref="alertSuccess">
-      <p slot="title">Sucesso!</p>
-      <p slot="message">
-        A notícia foi adicionada com sucesso!
-      </p>
-    </alert-success>
-    </q-card-section>
+          <p slot="title">Algo deu errado!</p>
+          <p slot="message">
+            Infelizmente não foi possivel adicionar a noticia no mural!
+          </p>
+        </alert-error>
+        <alert-success ref="alertSuccess">
+          <p slot="title">Sucesso!</p>
+          <p slot="message">
+            A notícia foi adicionada com sucesso!
+          </p>
+        </alert-success>
+      </q-card-section>
     </q-card>
   </div>
 </template>
@@ -160,31 +147,30 @@ export default {
     },
   },
 };
-
 </script>
 <style>
-
 .slide-fade-enter-active {
-  transition: all .3s ease;
+  transition: all 0.3s ease;
 }
 .slide-fade-leave-active {
-  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
 }
-.slide-fade-enter, .slide-fade-leave-to {
+.slide-fade-enter,
+.slide-fade-leave-to {
   transform: translateX(10px);
   opacity: 0;
 }
 
-.btnCadastrar{
+.btnCadastrar {
   margin-top: 40px;
 }
 
-.data-experacao{
+.data-experacao {
   margin-top: 12px;
   margin-left: 55px;
 }
 
-.check-box{
+.check-box {
   margin-top: 10px;
   margin-left: 55px;
 }
--- src/pages/newsfeed/NewsfeedList.vue
+++ src/pages/newsfeed/NewsfeedList.vue
@@ -1,52 +1,60 @@
 <template>
   <div>
     <q-card class="q-pa-xl min">
-    <q-card-section class="q-pt-none q-gutter-sm">
-       <div class="btnCadastrar-mural row justify-center">
-    <q-btn
-        label="CRIAR POSTAGEM"
-        class="q-mt-md"
-        color="teal"
-        @click="this.criarPostagem"
-        >
-      </q-btn>
-    </div>
-      <p class="text-h6 text-dark text-center svd-title">
-        Mural de notícias
-      </p>
-       <q-form class="row justify-center" >
-         <div class="col col-10 q-col-gutter-sm">
-      <ul class="lista-noticia">
-        <li v-for="news of newsfeed" :key="news">
-          <div class="painel">
-            <div class="painel-margem">
-              <div class="titulo-icon">
-                <h5 class="painel-conteudo-if" v-if="news.showIcon">
-            <img class="imagem" src="https://img.icons8.com/fluent/48/000000/bell.png"/>{{ news.title}}
-            </h5>
-            <h5 class="painel-conteudo-noticia" v-else-if="!news.showIcon">
-           {{ news.title}}
-            </h5>
-              </div>
-              <div class="painel-conteudo">
-                <h6 class="painel-conteudo-descricao">{{ news.description}}</h6>
-              </div>
-              <div>
-                <h8 class="painel-conteudo-link" v-if="!news.showIcon">ir para página
-                </h8>
-              </div>
-            </div>
+      <q-card-section class="q-pt-none q-gutter-sm">
+        <div class="btnCadastrar-mural row justify-center">
+          <q-btn
+            label="CRIAR POSTAGEM"
+            class="q-mt-md"
+            color="teal"
+            @click="this.criarPostagem"
+          >
+          </q-btn>
+        </div>
+        <p class="text-h6 text-dark text-center svd-title">
+          Mural de notícias
+        </p>
+        <q-form class="row justify-center">
+          <div class="col col-10 q-col-gutter-sm">
+            <ul class="lista-noticia">
+              <li v-for="news of newsfeed" :key="news">
+                <div class="painel">
+                  <div class="painel-margem">
+                    <div class="titulo-icon">
+                      <h5 class="painel-conteudo-if" v-if="news.showIcon">
+                        <img
+                          class="imagem"
+                          src="https://img.icons8.com/fluent/48/000000/bell.png"
+                        />{{ news.title }}
+                      </h5>
+                      <h5
+                        class="painel-conteudo-noticia"
+                        v-else-if="!news.showIcon"
+                      >
+                        {{ news.title }}
+                      </h5>
+                    </div>
+                    <div class="painel-conteudo">
+                      <h6 class="painel-conteudo-descricao">
+                        {{ news.description }}
+                      </h6>
+                    </div>
+                    <div>
+                      <h8 class="painel-conteudo-link" v-if="!news.showIcon"
+                        >ir para página
+                      </h8>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
-        </li>
-      </ul>
-      </div>
-      </q-form>
-    </q-card-section>
+        </q-form>
+      </q-card-section>
     </q-card>
   </div>
 </template>
 <script>
-
 export default {
   data() {
     return {
@@ -77,65 +85,63 @@ export default {
     },
   },
 };
-
 </script>
 <style>
-
 .slide-fade-enter-active {
-  transition: all .3s ease;
+  transition: all 0.3s ease;
 }
 .slide-fade-leave-active {
-  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
 }
-.slide-fade-enter, .slide-fade-leave-to {
+.slide-fade-enter,
+.slide-fade-leave-to {
   transform: translateX(10px);
   opacity: 0;
 }
-.btnCadastrar{
+.btnCadastrar {
   margin-top: 40px;
 }
 
-.lista-noticia-item{
+.lista-noticia-item {
   list-style: none;
 }
 
-.painel-conteudo-if{
+.painel-conteudo-if {
   color: blueviolet;
   font-size: 25px;
   font-weight: bold;
 }
-.imagem{
+.imagem {
   list-style: none;
 }
 
-.painel{
+.painel {
   border: solid 2px grey;
   border-radius: 10px;
   margin-bottom: 50px;
 }
 
-.painel .painel-margem{
-    margin: 8px;
-    margin-bottom: 8px;
+.painel .painel-margem {
+  margin: 8px;
+  margin-bottom: 8px;
 }
 
-.painel-conteudo-noticia{
+.painel-conteudo-noticia {
   list-style: none;
   font-size: 25px;
   font-weight: bold;
   margin-bottom: 8px;
 }
 
-
-.titulo-icon{
+.titulo-icon {
   list-style: none;
 }
 
-.btnCadastrar-mural{
+.btnCadastrar-mural {
   margin-inline-start: 500px;
 }
 
-.painel-conteudo-link{
+.painel-conteudo-link {
   color: blueviolet;
 }
 </style>
