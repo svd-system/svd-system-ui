52444e8ccaf7868c7b49d396b2487b2495cb3ffe
AlexsandroCastro <alexsandrocasttro@gmail.com>
2020-06-11 21:14:52 -0300

    Cadastrar e listar posts no newsfeed

---
+++ src/pages/newsfeed/NewsfeedData.vue
@@ -0,0 +1,191 @@
+<template>
+  <div>
+    <q-card class="q-pa-xl min">
+      <q-card-section>
+    <q-card-section class="q-pt-none q-gutter-sm">
+      <router-link class="text-primary" to="/user/newsfeedList" tag="a">
+        voltar para o mural
+      </router-link>
+      <p class="text-h6 text-dark text-center svd-title">
+        Adicionar nova notícia no mural
+      </p>
+    </q-card-section>
+      <q-form class="row justify-center" >
+        <div class="col col-10 q-col-gutter-sm">
+          <div class="corpo">
+            <label class="col col-xs-12 col-md-10">Titulo
+            <q-input
+              outlined
+              :dense="true"
+              v-model="newsfeed.title"
+            >
+            </q-input>
+            </label>
+          </div>
+        </div>
+      </q-form>
+      <q-form class="row justify-center" >
+        <div class="col col-10 q-col-gutter-sm">
+          <div class="corpo">
+            <label class="col col-xs-12 col-md-10">Descrição
+             <q-input
+              outlined
+              :dense="true"
+              v-model="newsfeed.description"
+            >
+            </q-input>
+            </label>
+          </div>
+        </div>
+      </q-form>
+      <q-form class="row justify-center" >
+        <div class="col col-10 q-col-gutter-sm">
+          <div class="corpo">
+            <label class="col col-xs-12 col-md-10">Link
+            <q-input
+              outlined
+              :dense="true"
+              v-model="newsfeed.link"
+            >
+            </q-input>
+            </label>
+          </div>
+        </div>
+      </q-form>
+      <q-form class="row justify-left" >
+        <div class="col col-5 q-col-gutter-sm">
+          <div class="data-experacao">
+            <label class="col col-xs-2 col-md-10">Data de expiração
+               <q-input
+                outlined
+              :dense="true"
+                type="date"
+                :locale="dateUtils.LOCALE"
+                v-model="newsfeed.expiresAt"
+                  />
+            </label>
+          </div>
+        </div>
+      </q-form>
+      <q-form class="row justify-left" >
+        <div class="col col-10 q-col-gutter-sm">
+          <div class="check-box">
+            <q-checkbox
+            label="Exibir ícone de notificação?"
+            :dense="true"
+             color="accent"
+             v-model="newsfeed.showIcon"
+           />
+          </div>
+        </div>
+      </q-form>
+    <div class="btnCadastrar row justify-center">
+    <q-btn
+        label="Criar"
+        class="q-mt-md"
+        color="teal"
+        @click="create"
+        >
+        <template v-slot:loading>
+          <q-spinner-facebook />
+        </template>
+      </q-btn>
+    </div>
+        <alert-error ref="alertError">
+      <p slot="title">Algo deu errado!</p>
+      <p slot="message">
+        Infelizmente não foi possivel adicionar a noticia no mural!
+      </p>
+    </alert-error>
+    <alert-success ref="alertSuccess">
+      <p slot="title">Sucesso!</p>
+      <p slot="message">
+        A notícia foi adicionada com sucesso!
+      </p>
+    </alert-success>
+    </q-card-section>
+    </q-card>
+  </div>
+</template>
+<script>
+import dateUtils from '../../shared/utils/date';
+import AlertError from '../../components/shared/alert/Error';
+import AlertSuccess from '../../components/shared/alert/Success';
+
+export default {
+  components: {
+    AlertError,
+    AlertSuccess,
+  },
+  data() {
+    return {
+      dateUtils,
+      newsfeed: {
+        title: '',
+        description: '',
+        link: '',
+        expiresAt: '',
+        showIcon: false,
+      },
+    };
+  },
+  methods: {
+    create() {
+      this.newsfeed = {
+        title: this.newsfeed.title,
+        description: this.newsfeed.description,
+        link: this.newsfeed.link,
+        expiresAt: dateUtils.toString(this.newsfeed.expiresAt),
+        showIcon: this.newsfeed.showIcon,
+      };
+
+      this.$axios
+        .post('/api/newsfeed', this.newsfeed)
+        .then(() => {
+          this.$refs.alertSuccess.open();
+          // eslint-disable-next-line no-console
+          console.log(this.newsfeed);
+          this.newsfeed = {
+            title: '',
+            description: '',
+            link: '',
+            expiresAt: '',
+          };
+        })
+        .catch(() => {
+          // eslint-disable-next-line no-console
+          console.log(this.newsfeed);
+          this.$refs.alertError.open();
+        });
+    },
+  },
+};
+
+</script>
+<style>
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+.btnCadastrar{
+  margin-top: 40px;
+}
+
+.data-experacao{
+  margin-top: 12px;
+  margin-left: 55px;
+}
+
+.check-box{
+  margin-top: 10px;
+  margin-left: 55px;
+}
+</style>
---
+++ src/pages/newsfeed/NewsfeedList.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <q-card class="q-pa-xl min">
+    <q-card-section class="q-pt-none q-gutter-sm">
+       <div class="btnCadastrar-mural row justify-center">
+    <q-btn
+        label="CRIAR POSTAGEM"
+        class="q-mt-md"
+        color="teal"
+        @click="this.criarPostagem"
+        >
+      </q-btn>
+    </div>
+      <p class="text-h6 text-dark text-center svd-title">
+        Mural de notícias
+      </p>
+       <q-form class="row justify-center" >
+         <div class="col col-10 q-col-gutter-sm">
+      <ul class="lista-noticia">
+        <li v-for="news of newsfeed" :key="news">
+          <div class="painel">
+            <div class="painel-margem">
+              <div class="titulo-icon">
+                <h5 class="painel-conteudo-if" v-if="news.showIcon">
+            <img class="imagem" src="https://img.icons8.com/fluent/48/000000/bell.png"/>{{ news.title}}
+            </h5>
+            <h5 class="painel-conteudo-noticia" v-else-if="!news.showIcon">
+           {{ news.title}}
+            </h5>
+              </div>
+              <div class="painel-conteudo">
+                <h6 class="painel-conteudo-descricao">{{ news.description}}</h6>
+              </div>
+              <div>
+                <h8 class="painel-conteudo-link" v-if="!news.showIcon">ir para página
+                </h8>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+      </div>
+      </q-form>
+    </q-card-section>
+    </q-card>
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {
+      newsfeed: [],
+    };
+  },
+  created() {
+    this.$axios
+      .get('/api/newsfeed/')
+      .then((response) => {
+        this.newsfeed = response.data;
+        // eslint-disable-next-line no-console
+        console.log(response);
+        // eslint-disable-next-line no-console
+        console.log(this.newsfeed);
+      })
+      .catch(() => {
+        // eslint-disable-next-line no-console
+        console.log(this.newsfeed);
+      });
+  },
+  methods: {
+    criarPostagem() {
+      this.$router.push('/user/newsfeed/new');
+    },
+    edit(newsfeedid) {
+      this.$router.push(`${newsfeedid}`);
+    },
+  },
+};
+
+</script>
+<style>
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
+.btnCadastrar{
+  margin-top: 40px;
+}
+
+.lista-noticia-item{
+  list-style: none;
+}
+
+.painel-conteudo-if{
+  color: blueviolet;
+  font-size: 25px;
+  font-weight: bold;
+}
+.imagem{
+  list-style: none;
+}
+
+.painel{
+  border: solid 2px grey;
+  border-radius: 10px;
+  margin-bottom: 50px;
+}
+
+.painel .painel-margem{
+    margin: 8px;
+    margin-bottom: 8px;
+}
+
+.painel-conteudo-noticia{
+  list-style: none;
+  font-size: 25px;
+  font-weight: bold;
+  margin-bottom: 8px;
+}
+
+
+.titulo-icon{
+  list-style: none;
+}
+
+.btnCadastrar-mural{
+  margin-inline-start: 500px;
+}
+
+.painel-conteudo-link{
+  color: blueviolet;
+}
+</style>
